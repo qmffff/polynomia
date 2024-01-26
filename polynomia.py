9977280aff71e14d76ce75784f82c5a0c076f477
qmffff <qianminfeng2000@gmail.com>
2024-01-26 16:53:55 -0500

    cs 506 exercise part b

--- polynomia.py
+++ polynomia.py
@@ -23,20 +23,81 @@ class Add:
         return repr(self.p1) + " + " + repr(self.p2)
 
 
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Mul):
+            left = "( " + repr(self.p1) + " )"
+        else:
+            left = repr(self.p1)
+
+        if isinstance(self.p2, Add) or isinstance(self.p2, Mul) or isinstance(self.p2, Div):
+            right = "( " + repr(self.p2) + " )"
+        else:
+            right = repr(self.p2)
+
+        return left + " / " + right
+
+
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        if isinstance(self.p1, Add) or isinstance(self.p1, Mul):
+            left = "( " + repr(self.p1) + " )"
+        else:
+            left = repr(self.p1)
+
+        if isinstance(self.p2, Add) or isinstance(self.p2, Mul):
+            right = "( " + repr(self.p2) + " )"
+        else:
+            right = repr(self.p2)
+
+        return left + " - " + right
+
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        if isinstance(self.p1, (Add, Sub, Div)):
+            left = "( " + repr(self.p1) + " )"
+        else:
+            left = repr(self.p1)
+
+        if isinstance(self.p2, (Add, Sub, Div)):
+            right = "( " + repr(self.p2) + " )"
+        else:
+            right = repr(self.p2)
+
+        return left + " * " + right
 
 
-poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+poly = Add(
+    Sub(
+        Mul(
+            Int(3),
+            Div(X(), Int(2))
+        ),
+        Int(5)
+    ),
+    Mul(
+        Int(4),
+        Add(
+            X(),
+            Div(
+                Mul(X(), X()),
+                Int(3)
+            )
+        )
+    )
+)
+
 print(poly)
